c64fbd0044d57049ed33f19cc0c3dc4ca199aa6e
Pavankumardontha <pavankumardontha@gmail.com>
2021-01-27 16:37:43 +0530

    Add files via upload

---
+++ fftw3import.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+#include<iostream>
+#include<fftw3.h> //note this syntax carefully
+using namespace std;
+
+int main()
+{
+   cout<<"hello world"<<endl;
+}
+ 
+/* we should first use the syntax g++ op1.cpp and then ./a.out for compilation.*/
